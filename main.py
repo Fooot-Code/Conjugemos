35a599e34da6256bc59324eb5e01ec7278132ecc
oof15642 <59772241+oof15642@users.noreply.github.com>
2024-02-15 07:28:53 -0700

    it is right!

--- main.py
+++ main.py
@@ -2,60 +2,68 @@ import time
 import cv2
 import mss
 import numpy
+import keyboard
 import pytesseract
+import pyautogui
+from pynput.mouse import Controller
+
+
+
+
+
 
 # Define the screen region to capture
 mon = {'top': 275, 'left': 700, 'width': 600, 'height': 100}
 
 dictENGtoESP = {
-    "The avenue": "La avenida",
+    "avenue": "La avenida",
     "truck": "El camión",
-    "The highway": "La carretera",
-    "The driver (male/female)": "El conductor/la conductora",
-    "The street crossing": "El cruce de calles",
-    "The block": "La cuadra",
-    "The corner": "La esquina",
-    "The statue": "La estatua",
-    "The fountain": "La fuente",
-    "The pedestrian/pedestrians": "El peatón/los peatones",
+    "highway": "La carretera",
+    "driver/conductor": "El conductor",
+    "intersection": "El cruce de calles",
+    "block": "La cuadra",
+    "corner": "La esquina",
+    "statue": "La estatua",
+    "fountain": "La fuente",
+    "pedestrian": "El peatón",
     "driver's license": "El permiso de manejar",
-    "The square": "La plaza",
-    "The police officer (male/female)": "El policía, la policía",
-    "To give a fine": "Poner una multa",
-    "The bridge": "El puente",
-    "The traffic light": "El semáforo",
-    "The stop sign": "La señal de parada",
+    "plaza": "La plaza",
+    "police": "El policía",
+    "To give a ticket": "Poner una multa",
+    "bridge": "El puente",
+    "stoplight": "El semáforo",
+    "stop sign": "La señal de parada",
     "Traffic": "El tráfico",
-    "wide": "ancho/a",
+    "wide": "ancho",
     "Enough!": "¡Basta!",
     "Agree": "De acuerdo.",
     "To leave": "Dejar",
     "Leave me alone": "Déjame en paz",
     "Slowly": "Despacio",
     "To wait": "Esperar",
-    "To be sure": "Estar seguro/a",
-    "narrow": "estrecho/a",
-    "You are making me nervous": "Me estás poniendo nervioso/a",
-    "dangerous": "peligroso/a",
+    "To be sure": "Estar seguro",
+    "tight": "estrecho",
+    "You are making me nervous": "Me estás poniendo nervioso",
+    "dangerous": "peligroso",
     "To remove": "Quitar",
     "To be careful": "Tener cuidado",
     "already": "ya",
     "Approximately": "Aproximadamente",
     "How do you get to...?": "¿Cómo se va…?",
-    "complicated": "complicado/a",
+    "complicated": "complicado",
     "To cross": "Cruzar",
     "Straight": "Derecho",
-    "From": "Desde",
+    "since": "Desde",
     "To turn": "Doblar",
     "In the middle of": "En medio de",
     "Until": "Hasta",
     "To drive": "manejar",
-    "The subway": "El metro",
+    "metro": "El metro",
     "To stop": "Parar",
     "To pass": "Pasar",
-    "Through/by/for": "Por",
+    "Through/by": "Por",
     "To stay": "Quedar",
-    "To follow": "Seguir (e→i)",
+    "To follow": "Seguir",
     "To be in a hurry": "Tener prisa"
 }
 
@@ -66,12 +74,38 @@ with mss.mss() as sct:
         text = pytesseract.image_to_string(im)
         cv2.imshow('Image', im)
 
+        # Create a mouse controller
+        # mouse = Controller()
+
+        # Get the mouse position
+        # x, y = mouse.position
+        # print(f"Mouse position: X = {x}, Y = {y}")
+        
+        # Get the mouse position
+        # Get the desired coordinates (replace with your own)
+        target_x1, target_y2 = 964, 452
+
+        # Move the mouse to the desired location
+        pyautogui.moveTo(target_x1, target_y2, duration=0.5)
+
+        # Perform a left mouse click
+        pyautogui.click()
+
         for keys in dictENGtoESP:
             if keys.lower() == text.rstrip().lower():
                 print(dictENGtoESP[keys])
+                keyboard.write(dictENGtoESP[keys])
+
+        target_x3, target_y4 = 1154, 607
+
+        # Move the mouse to the desired location
+        pyautogui.moveTo(target_x3, target_y4, duration=0.5)
+
+        # Perform a left mouse click
+        pyautogui.click()
 
         # Press "q" to quit
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
-        time.sleep(1)  # Capture one screenshot per second
+        time.sleep(5)  # Capture one screenshot per second
