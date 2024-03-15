1554e156be41747117f5d3e85e7e3d15953f4513
oof15642 <59772241+oof15642@users.noreply.github.com>
2024-03-14 23:48:50 -0600

    Ran Num and Toys Now
    Added the random number to get 1 wrong. Also updated the dictionary

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import keyboard
 import pytesseract
 import pyautogui
 from pynput.mouse import Controller
-
+from random import randint, choice
 
 
 
@@ -16,55 +16,44 @@ from pynput.mouse import Controller
 mon = {'top': 275, 'left': 700, 'width': 600, 'height': 100}
 
 dictENGtoESP = {
-    "avenue": "La avenida",
-    "truck": "El camión",
-    "highway": "La carretera",
-    "driver/conductor": "El conductor",
-    "intersection": "El cruce de calles",
-    "block": "La cuadra",
-    "corner": "La esquina",
-    "statue": "La estatua",
-    "fountain": "La fuente",
-    "pedestrian": "El peatón",
-    "driver's license": "El permiso de manejar",
-    "plaza": "La plaza",
-    "police": "El policía",
-    "To give a ticket": "Poner una multa",
-    "bridge": "El puente",
-    "stoplight": "El semáforo",
-    "stop sign": "La señal de parada",
-    "Traffic": "El tráfico",
-    "wide": "ancho",
-    "Enough!": "¡Basta!",
-    "Agree": "De acuerdo.",
-    "To leave": "Dejar",
-    "Leave me alone": "Déjame en paz",
-    "Slowly": "Despacio",
-    "To wait": "Esperar",
-    "To be sure": "Estar seguro",
-    "tight": "estrecho",
-    "You are making me nervous": "Me estás poniendo nervioso",
-    "dangerous": "peligroso",
-    "To remove": "Quitar",
-    "To be careful": "Tener cuidado",
-    "already": "ya",
-    "Approximately": "Aproximadamente",
-    "How do you get to...?": "¿Cómo se va…?",
-    "complicated": "complicado",
-    "To cross": "Cruzar",
-    "Straight": "Derecho",
-    "since": "Desde",
-    "To turn": "Doblar",
-    "In the middle of": "En medio de",
-    "Until": "Hasta",
-    "To drive": "manejar",
-    "metro": "El metro",
-    "To stop": "Parar",
-    "To pass": "Pasar",
-    "Through/by": "Por",
-    "To stay": "Quedar",
-    "To follow": "Seguir",
-    "To be in a hurry": "Tener prisa"
+    "action figure": "el muñeco",
+    "as achild": "de niño/a",
+    "as a child or when | was little": "de pequeño/a",
+    "blocks": "los bloques",
+    "coin": "la moneda",
+    "collection": "la colección",
+    "daycare center": "la guardería infantil",
+    "disobedient": "desobediente",
+    "doll": "la muñeca",
+    "electric train": "el tren eléctrico",
+    "everyone": "todo el mundo",
+    "fish": "el pez",
+    "generous": "generoso/a",
+    "in general": "por lo general",
+    "naughty": "travieso/a",
+    "neighbor": "el vecino/la vecina",
+    "obedient": "obediente",
+    "once in a while": "de vez en cuando",
+    "playground": "el patio de recreo",
+    "rope": "la cuerda",
+    "spoiled": "consentido/a",
+    "teddy bear": "el oso de peluche",
+    "timid": "tímido/a",
+    "to behave badly": "portarse mal",
+    "to behave well": "portarse bien",
+    "to bother": "molestar",
+    "to collect": "coleccionar",
+    "to fight": "pelearse",
+    "to jump rope": "saltar a la cuerda",
+    "to lie": "mentir",
+    "to obey": "obedecer",
+    "to offer": "ofrecer",
+    "to permit": "permitir",
+    "tricycle": "el triciclo",
+    "truth": "la verdad",
+    "turtle": "la tortuga",
+    "well-behaved": "bien educado/a",
+    "world": "el mundo"
 }
 
 with mss.mss() as sct:
@@ -72,8 +61,7 @@ with mss.mss() as sct:
         im = numpy.asarray(sct.grab(mon))
         im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         text = pytesseract.image_to_string(im)
-        cv2.imshow('Image', im)
-
+        #cv2.imshow('Image', im)
         # Create a mouse controller
         # mouse = Controller()
 
@@ -83,29 +71,56 @@ with mss.mss() as sct:
         
         # Get the mouse position
         # Get the desired coordinates (replace with your own)
-        target_x1, target_y2 = 964, 452
+        ansBoxX, ansBoxY = 964, 452
+        submitButtonX, submitButtonY = 1154, 607
 
         # Move the mouse to the desired location
-        pyautogui.moveTo(target_x1, target_y2, duration=0.5)
+        pyautogui.moveTo(ansBoxX, ansBoxY, duration=0.5)
 
         # Perform a left mouse click
         pyautogui.click()
 
+        print(text.rstrip().lower())
         for keys in dictENGtoESP:
             if keys.lower() == text.rstrip().lower():
                 print(dictENGtoESP[keys])
-                keyboard.write(dictENGtoESP[keys])
+                if randint(0, 100) < 86:
+                    keyboard.write(dictENGtoESP[keys])
 
-        target_x3, target_y4 = 1154, 607
+                    # Move the mouse to the desired location
+                    pyautogui.moveTo(submitButtonX, submitButtonY, duration=0.5)
 
-        # Move the mouse to the desired location
-        pyautogui.moveTo(target_x3, target_y4, duration=0.5)
+                    # Perform a left mouse click
+                    pyautogui.click()
+                else:
+                    dictList = list(dictENGtoESP.keys())
+                    keyboard.write(dictENGtoESP[choice(dictList)])
+                    pyautogui.moveTo(submitButtonX, submitButtonY, duration=0.5)
+                    pyautogui.click()
+                    
+                    pyautogui.moveTo(ansBoxX, ansBoxY, duration=0.5)
+                    pyautogui.click()
+                    keyboard.press_and_release("ctrl+a")
+                    keyboard.write("\b")
 
-        # Perform a left mouse click
-        pyautogui.click()
+                    keyboard.write(dictENGtoESP[keys])
+                    pyautogui.moveTo(submitButtonX, submitButtonY, duration=0.5)
+                    pyautogui.click()
+
+                    # Move the mouse to the desired location
+                    
+
+                    # Perform a left mouse click
+                    
+
+                    
+
+        
+
+        
 
         # Press "q" to quit
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
-        time.sleep(5)  # Capture one screenshot per second
+        time.sleep(15)  # Capture one screenshot per second
